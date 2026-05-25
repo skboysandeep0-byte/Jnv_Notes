bbac47d8c40a46de7179abe4eaad25d45e96dd92
skboysandeep0-byte <skboysandeep0@gmail.com>
2026-05-25 23:37:04 +0530

    Add timetable_screen.dart

---
+++ lib/screens/timetable_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class TimetableScreen extends StatelessWidget {
+  const TimetableScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Timetable')),
+      body: const Center(child: Text('Timetable Screen')),
+    );
+  }
+}
